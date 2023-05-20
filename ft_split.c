3f9282f5e0849fc4b58c91c2a3f6c872cafc3880
K4-R4 <mabdman512@gmail.com>
2023-05-21 01:38:41 +0900

    split tmp

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,14 @@
 /*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 19:33:19 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/05/20 18:43:45 by tkuramot         ###   ########.fr       */
+/*   Updated: 2023/05/21 01:29:15 by tkuramot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-static size_t	get_substr_count(char const *s, char c)
+static size_t	get_substr_num(char const *s, char c)
 {
 	int		is_word;
 	size_t	cnt;
@@ -33,57 +34,58 @@ static size_t	get_substr_count(char const *s, char c)
 	return (cnt);
 }
 
-static size_t	get_substr_length(char const *s, char c)
+char	**free_all(char **result, size_t len)
 {
-	size_t	len;
+	char	**tmp;
 
-	len = 0;
-	while (*s != '\0' && *s != c)
-	{
-		len++;
-		s++;
-	}
-	return (len);
+	tmp = result;
+	while (len-- > 0)
+		free(*tmp++);
+	free(result);
+	return (NULL);
 }
 
-static char	*extract_substr(char const *s, char c)
+static char	**ft_split_helper(char **result, char const *s, char c)
 {
-	char	*ret;
-	char	*tmp;
+	char	**tmp;
+	size_t	len;
 
-	ret = (char *)malloc(sizeof(char) * (get_substr_length(s, c) + 1));
-	if (ret == NULL)
-		return (NULL);
-	tmp = ret;
-	while (*s != '\0' && *s != c)
-		*tmp++ = *s++;
-	*tmp = '\0';
-	return (ret);
+	tmp = result;
+	while (s != NULL && *s != '\0')
+	{
+		while (*s == c)
+			s++;
+		if (*s == '\0')
+			break ;
+		len = ft_strchr(s, c) - s;
+		*tmp = ft_substr(s, 0, len);
+		if (*tmp == NULL)
+			return (free_all(result, (tmp - result) / sizeof(char *)));
+		s += len;
+		tmp++;
+	}
+	return (result);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**ret;
-	char	**tmp;
-	size_t	substr_cnt;
+	char	**result;
 
 	if (s == NULL)
 		return (NULL);
-	substr_cnt = get_substr_count(s, c);
-	ret = (char **)malloc(sizeof(char *) * (substr_cnt + 1));
-	if (ret == NULL)
+	result = ft_calloc(get_substr_num(s, c) + 1, sizeof(char *));
+	if (result == NULL)
 		return (NULL);
-	tmp = ret;
-	while (substr_cnt != 0 && *s != '\0')
+	return (ft_split_helper(result, s, c));
+}
+
+int	main(void)
+{
+	char s[] = "123444323234s";
+	char **res = ft_split(s, ' ');
+	while(*res != NULL)
 	{
-		while (*s != '\0' && *s == c)
-			s++;
-		*tmp++ = extract_substr(s, c);
-		while (*s != '\0' && *s != c)
-			s++;
-		while (*s != '\0' && *s == c)
-			s++;
+		printf("%s\n", *res++);
 	}
-	*tmp = NULL;
-	return (ret);
+	return (0);
 }
