aa8ef198cfe46ba5d58c5840d36c07f94802e09f
tkuramot <mabdman512@gmail.com>
2024-07-26 01:29:23 +0900

    fix find

--- dlist/ft_dlstfind.c
+++ dlist/ft_dlstfind.c
@@ -6,20 +6,21 @@
 /*   By: kura <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 01:02:07 by kura              #+#    #+#             */
-/*   Updated: 2024/07/26 01:16:17 by kura             ###   ########.fr       */
+/*   Updated: 2024/07/26 01:28:47 by kura             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_dlist.h"
 
-t_dlist	*ft_dlstfind(t_dlist *list, int (*find)(void *content))
+t_dlist	*ft_dlstfind(t_dlist *list, void *expected, int (*find)(void *actual,
+			void *expected))
 {
 	t_dlist	*tmp;
 
 	tmp = list;
 	while (tmp)
 	{
-		if (find(tmp->content))
+		if (find(tmp->content, expected))
 			return (tmp);
 		tmp = tmp->next;
 	}
